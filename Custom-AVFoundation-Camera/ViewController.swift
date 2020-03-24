7b29524b860b1d42947518bf3fe1204a1fca6ff0
Edward Prokopik <ejprok@gmail.com>
2020-03-24 16:47:57 -0700

    Add a preview layer for viewing the camera input

--- Custom-AVFoundation-Camera/ViewController.swift
+++ Custom-AVFoundation-Camera/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let session = AVCaptureSession()
+    var camera: AVCaptureDevice?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var cameraCaptureOutput: AVCapturePhotoOutput?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initCaptureSession()
+    }
+
+    func initCaptureSession() {
+        session.sessionPreset = AVCaptureSession.Preset.high
+        camera = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            if let availableCamera = camera {
+                let cameraCaptureInput = try AVCaptureDeviceInput(device: availableCamera)
+                cameraCaptureOutput = AVCapturePhotoOutput()
+
+                session.addInput(cameraCaptureInput)
+                session.addOutput(cameraCaptureOutput!)
+
+            } else {
+                print("no available camera")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        cameraPreviewLayer?.frame = view.bounds
+
+        if let layer = cameraPreviewLayer {
+            view.layer.insertSublayer(layer, at: 0)
+        }
+        
+        session.startRunning()
+
+
+
+
     }
 
 
