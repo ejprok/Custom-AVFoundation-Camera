a37d8f048eed99b756f15f82573a748a0aeb5dcb
Edward Prokopik <ejprok@gmail.com>
2020-03-24 16:29:25 -0700

    initial commit

---
+++ Custom-AVFoundation-Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom-AVFoundation-Camera
+//
+//  Created by Edward Prokopik on 3/24/20.
+//  Copyright © 2020 Edward Prokopik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Custom-AVFoundation-CameraTests/Custom_AVFoundation_CameraTests.swift
@@ -0,0 +1,34 @@
+//
+//  Custom_AVFoundation_CameraTests.swift
+//  Custom-AVFoundation-CameraTests
+//
+//  Created by Edward Prokopik on 3/24/20.
+//  Copyright © 2020 Edward Prokopik. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_AVFoundation_Camera
+
+class Custom_AVFoundation_CameraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
